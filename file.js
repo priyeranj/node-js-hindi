d2544abc8a26b4e42ff4c9feaaecf54565809f8a
Priyaranjan Kumar <priyaranjan.moscow@gmail.com>
2023-10-01 17:54:50 +0530

    some basics stuff

---
+++ file.js
@@ -0,0 +1,4 @@
+const fs = require("fs")
+
+
+fs.writeFileSync('./test.txt',"Hey There");
--- hello.js
+++ hello.js
@@ -1,6 +1,8 @@
 // const math = require('./math')
 
-const {add,sub}=require('./math')//require is used to import other js module/file so that we can use functions of that module in current file.
+const math=require("https")//require is used to import other js module/file so that we can use functions of that module in current file.
 
-console.log(add(9,7));
-console.log(sub(9,7));
+// console.log(add(9,7));
+// console.log(sub(9,7));
+
+console.log(math);
