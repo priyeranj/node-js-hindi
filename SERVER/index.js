6d537fd509f8105c7296fd8cebd2c89e40f2796c
Priyaranjan Kumar <priyaranjan.moscow@gmail.com>
2023-10-02 12:39:11 +0530

    installed express

--- SERVER/index.js
+++ SERVER/index.js
@@ -4,22 +4,31 @@ const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.url} New req received\n`;
+  const log = `${Date.now()}: ${req.method} ${req.url} New req received\n`;
   const myUrl = url.parse(req.url, true);
   console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
     switch (myUrl.pathname) {
       case "/":
-        res.end("Home Page");
+        if (req.method === "GET") res.end("HomePage");
         break;
       case "/about":
         const username = myUrl.query.myName;
         res.end(`Hi,${username}`);
         break;
       case "/search":
-         const search = myUrl.query.search_query;
-         res.end("Here are your results for "+search);
-         break;  
+        const search = myUrl.query.search_query;
+        res.end("Here are your results for " + search);
+        break;
+      case "/signup":
+        if(req.method==="GET") res.end("This is a signup form")
+        else{
+            if(res.method==="POST"){
+                //DB Query
+                res.end("Success");
+            }
+        }  
+        break;
       default:
         res.end("404 file not found");
     }
